cf7b5cc83b9c77a778230f561e9e8487552a6080
francis-carroll <c00226918@itcarlow.ie>
2020-09-28 12:30:16 +0100

    added Modulo3 version 0.2.0

---
+++ Assets/Scripts/Modulo3Filter.cs
@@ -0,0 +1,17 @@
+﻿public class Modulo3Filter
+{
+    public static int modulo3(int input, int devisor)
+	{
+		return input - ((input / devisor) * devisor);
+	}
+
+	public static int[] modulo3s(int[] input)
+	{
+		int[] result = new int[input.Length];
+		for (int i = 0; i < input.Length; i++)
+		{
+			result[i] = modulo3(input[i], 3);
+		}
+		return result;
+	}
+}
---
+++ Assets/Tests/Modulo3FilterTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class Modulo3FilterTest
+    {
+        [Test]
+        public void Modulo3FilterTestSimple()
+        {
+            int[] input = { 7, 2, 8, 3, 9, 4, 10, 5, 11, 6 };
+            int[] expected = Modulo3Filter.modulo3s(input);
+            int[] output = { 1, 2, 2, 0, 0, 1, 1, 2, 2, 0 };
+
+            CollectionAssert.AreEqual(expected, output);
+        }
+    }
+}
